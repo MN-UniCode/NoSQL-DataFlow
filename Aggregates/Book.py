29e5cae42b3214761e014a58f447cbe2ba286553
Maurizio-Meschi <maurizio.meschi@gmail.com>
2025-01-22 17:42:46 +0100

    API: informations about books V1.1

---
+++ Aggregates/Book.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# Create the pandas dataframe
+book_data = {
+    "id": None,  # To be updated dynamically
+    "description": None,  # To be updated dynamically
+    "release_year": None,  # To be updated dynamically
+    "title": None,  # To be updated dynamically
+    "publisherId" : None,
+    "reviews": []
+}
+
+df = pd.DataFrame(book_data)
+
+
+def generate_books_csv(response):
+    
+    data = response.json()  # Parsed JSON response
+    
+    # Extract the books informations
+    books = data.get("data", {}).get("books", [])
+    # Get book and add the iformation to the dataframe
+    for book in books:
+        id = book.get("id", None)
+        description = book.get("description", None)
+        release_year = book.get("release_year", None)
+        title = book.get("title", None)
+        recommendations = book.get("recommendations", None)
+
+        # Add the userId to reviews in according to Cassandra schema
+        for i in range(0, len(recommendations)-1):
+            #TODO: find the correct userId
+            recommendations[i]["userId"] = None
+        
+        #TODO: find the correct publisherId
+        df.loc[len(df)] = [id, description, release_year, title, 1, recommendations]
+        #print(f"recommendations: {recommendations}")
+
+    df.to_csv("books.csv", index=False)
+    print(df)
--- Manager/API_Manager.py
+++ Manager/API_Manager.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from config.config import API_KEY
+import Aggregates.Book as AB
  
 url = "https://api.hardcover.app/v1/graphql"
 headers = {
@@ -25,42 +26,9 @@ query MyQuery {
 }
 """
 
-# Create the pandas dataframe
-book_data = {
-    "id": None,  # To be updated dynamically
-    "description": None,  # To be updated dynamically
-    "release_year": None,  # To be updated dynamically
-    "title": None,  # To be updated dynamically
-    "reviews": []
-}
-
-df = pd.DataFrame(book_data)
-
 # Sending the request
 response = requests.post(url, json={"query": query}, headers=headers)
-
-# Checking the response
 if response.status_code == 200:
-    data = response.json()  # Parsed JSON response
-    
-    # Extract the books informations
-    books = data.get("data", {}).get("books", [])
-    # Get book and add the iformation to the dataframe
-    for book in books:
-        id = book.get("id", None)
-        description = book.get("description", None)
-        release_year = book.get("release_year", None)
-        title = book.get("title", None)
-        recommendations = book.get("recommendations", None)
-
-        # Add the userId to reviews in according to Cassandra schema
-        for i in range(0, len(recommendations)-1):
-            recommendations[i]["userId"] = None
-        
-        df.loc[len(df)] = [id, description, release_year, title, recommendations]
-        #print(f"recommendations: {recommendations}")
+    AB.generate_books_csv(response)
 else:
     print(f"Error: {response.status_code}, {response.text}")
-
-
-print(df)
